542175b66ab4049c54b9cccf221026c7f34dedfe
1024735339@qq.com <1024735339@qq.com>
2020-03-26 00:14:10 +0800

    文字粒子效果

--- js/recursion.js
+++ js/recursion.js
@@ -1,11 +1,11 @@
 console.log("---------------------------------------");
 
 function sum (n) {
-	if (n === 1) {
-		return n;
-	} else {
-		return (n + sum(n - 1));
-	}
+    if (n === 1) {
+        return n;
+    } else {
+        return (n + sum(n - 1));
+    }
 }
 console.time("递归求和");
 console.log(sum(1));
@@ -15,11 +15,11 @@ console.log(sum(100));
 console.timeEnd("递归求和");
 
 function sum2 (n) {
-	var result = 0;
-	for (let i = 1; i <= n; i++) {
-		result += i;
-	}
-	return result;
+    var result = 0;
+    for (let i = 1; i <= n; i++) {
+        result += i;
+    }
+    return result;
 }
 console.time("循环求和");
 console.log(sum2(1));
@@ -29,11 +29,11 @@ console.log(sum2(100));
 console.timeEnd("循环求和");
 
 function factorial (n) {
-	if (n === 1) {
-		return n;
-	} else {
-		return (n * factorial(n - 1));
-	}
+    if (n === 1) {
+        return n;
+    } else {
+        return (n * factorial(n - 1));
+    }
 }
 console.time("递归阶乘");
 console.log(factorial(1));
@@ -43,11 +43,11 @@ console.log(factorial(100));
 console.timeEnd("递归阶乘");
 
 function factorial2 (n) {
-	var result = 1;
-	for (let i = 1; i <= n; i++) {
-		result *= i;
-	}
-	return result;
+    var result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 console.time("循环阶乘");
 console.log(factorial2(1));
